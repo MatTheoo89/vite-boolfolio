c2ec6904d7379265925f03ac1bee7b2aa145de8e
Mattheoo89 <matteobellomo.dev@gmail.com>
2023-01-26 15:37:28 +0100

    [add] Milestone 3

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  
+  <div class="container">
+    <div class="row">
+      <div class="col2">
+        <h1>co dio</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss">
+  @use './styles/general';
+</style>
