6c6a2605ed03a96f821283ad1047de238f2c01e3
Mattheoo89 <matteobellomo.dev@gmail.com>
2023-01-26 17:53:28 +0100

    [add] Milestone 5

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
 export default {
+    components: { ProjectCard },
   
   name: 'App',
   
@@ -15,7 +17,7 @@ export default {
     getApi(){
       axios.get(this.baseUrl + this.ProjectUri)
       .then(r => {
-        this.projects = r.data;
+        this.projects = r.data.projects;
           console.log(this.projects);
       })
       // .catch( error => {errorMsg = error})
@@ -37,6 +39,11 @@ export default {
     <div class="row">
       <div class="col2">
         <h1>PHP è più bello, tanto più bello!! Buuuuu Vue</h1>
+      </div>
+      <div class="col">
+        <div class="container-fluid d-flex flex-wrap justify-content-around">
+        <project-card class="m-3" v-for="project in projects" :key="project.id" :project="project"/>
+        </div>
       </div>
     </div>
   </div>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="card col-3 text-black">
+        <img src="{{project.cover_image}}" class="card-img-top" :alt="project.name">
+        <div class="card-body">
+        <h3 class="card-title fw-bold">{{project.name}}</h3>
+        <h4 class="card-title"> <strong>Cateoria:</strong> {{project.type.type}}</h4>
+        <h5 class="card-title" v-if="project.technologies.length">
+            <strong>Tecnologie: </strong>
+            <span class="badge text-bg-danger me-2"
+                    v-for="tag in project.technologies" :key="tag.id">{{tag.type}}</span>
+        </h5>
+        <p class="card-text">{{project.summary}}</p>
+        <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+    </div>
+</div>
+    
+</template>
+
+<script>
+export default {
+    name: 'ProjectCard',
+    props:{
+        project: Object,
+    }
+}
+</script>
+
+<style>
+
+</style>
