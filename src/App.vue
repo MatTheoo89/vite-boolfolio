c7135bfc9ba97afdd7285000f2f8127d534ac03d
Mattheoo89 <matteobellomo.dev@gmail.com>
2023-01-26 16:58:27 +0100

    [add] millestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <script>
+import axios from 'axios';
 export default {
-
+  
+  name: 'App',
+  
+  data(){
+    return {
+      baseUrl: 'http://127.0.0.1:8000/api',
+      ProjectUri: '/project',
+      projects: [],
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(this.baseUrl + this.ProjectUri)
+      .then(r => {
+        this.projects = r.data;
+          console.log(this.projects);
+      })
+      // .catch( error => {errorMsg = error})
+    },
+  },
+    mounted(){
+      this.getApi();
+      // console.log('mounted');
+    },
+    created(){
+      // console.log('created');
+    }
 }
 </script>
 
 <template>
   
-  <div class="container">
+  <div class="container my-5">
     <div class="row">
       <div class="col2">
-        <h1>co dio</h1>
+        <h1>PHP è più bello, tanto più bello!! Buuuuu Vue</h1>
       </div>
     </div>
   </div>
